4c12d5ed6ed73566723dc923d364e5d4bfb22069
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-15 16:59:21 -0500

    feat: add bottom bar item

---
+++ app/src/main/java/com/example/streamingapp/presentation/ui/navigation/BottomBarItem.kt
@@ -0,0 +1,24 @@
+package com.example.streamingapp.presentation.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomBarItem (
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+) {
+    object Home: BottomBarItem(
+        route = "home",
+        title = "Home",
+        icon =  Icons.Default.Home
+    )
+
+    object Player: BottomBarItem(
+        route = "player",
+        title = "Player",
+        icon = Icons.Default.PlayArrow
+    )
+}
