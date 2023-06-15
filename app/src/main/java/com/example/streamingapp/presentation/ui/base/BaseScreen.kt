590f148505900cd585b631927fe3b79a90267434
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-15 16:59:03 -0500

    feat: add base screen

---
+++ app/src/main/java/com/example/streamingapp/presentation/ui/base/BaseScreen.kt
@@ -0,0 +1,108 @@
+package com.example.streamingapp.presentation.ui.base
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.streamingapp.R
+import com.example.streamingapp.presentation.ui.navigation.BottomBarItem
+import com.example.streamingapp.presentation.ui.navigation.graphs.HomeNavGraph
+import com.example.streamingapp.presentation.ui.theme.StreamingAppTheme
+
+@Composable
+fun BaseScreen() {
+    BaseScreenContent()
+}
+
+@Composable
+fun BaseScreenContent() {
+    val navController: NavHostController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomBar(navController = navController) }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            HomeNavGraph(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarItem.Home,
+        BottomBarItem.Player
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    val bottomBarDestination = screens.any { it.route == currentDestination?.route }
+    if (bottomBarDestination) {
+        NavigationBar(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        ) {
+            screens.forEach { item ->
+                AddItem(
+                    item = item,
+                    currentDestination = currentDestination,
+                    navController = navController
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun RowScope.AddItem(
+    item: BottomBarItem,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    NavigationBarItem(
+        label = {
+            Text(
+                text = item.title,
+                style = MaterialTheme.typography.labelLarge
+            )
+        },
+        icon = {
+            Icon(
+                imageVector = item.icon,
+                contentDescription = stringResource(R.string.bottom_navigation_icon_description)
+            )
+        },
+        selected = currentDestination?.hierarchy?.any {
+            it.route == item.route
+        } == true,
+        onClick = {
+            navController.navigate(item.route) {
+                popUpTo(navController.graph.findStartDestination().id)
+                launchSingleTop = true
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun BaseScreenPreview() {
+    StreamingAppTheme {
+        BaseScreenContent()
+    }
+}
