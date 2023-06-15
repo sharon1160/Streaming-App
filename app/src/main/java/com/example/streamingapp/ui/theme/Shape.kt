8359127f57715020e8446ce7fc5990cd14e792ec
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-15 15:11:03 -0500

    feat: add shapes

---
+++ app/src/main/java/com/example/streamingapp/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+package com.example.streamingapp.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(16.dp),
+    extraLarge = RoundedCornerShape(24.dp)
+)
