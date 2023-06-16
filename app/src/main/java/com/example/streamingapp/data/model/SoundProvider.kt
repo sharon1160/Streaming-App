e69e63526f986a2a499b0e1c4f0171c4b6eede97
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-16 12:19:23 -0500

    feat: add sound provider

---
+++ app/src/main/java/com/example/streamingapp/data/model/SoundProvider.kt
@@ -0,0 +1,8 @@
+package com.example.streamingapp.data.model
+
+class SoundProvider {
+    companion object {
+        var sounds: List<Sound> = emptyList()
+        var detailedSound: DetailedSound? = null
+    }
+}
