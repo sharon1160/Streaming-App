099ff45f0fa002acc252c59a8bb51d2a3193a22c
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-19 11:55:53 -0500

    feat: add home view model using hilt

--- app/src/main/java/com/example/streamingapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/streamingapp/ui/home/HomeScreen.kt
@@ -3,10 +3,15 @@ package com.example.streamingapp.ui.home
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.streamingapp.ui.theme.StreamingAppTheme
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    homeViewModel: HomeViewModel = hiltViewModel(),
+) {
+    homeViewModel.searchAllSounds("guitarra")
+    homeViewModel.searchSoundById(516693)
     StreamingAppTheme {
         HomeScreenContent()
     }
