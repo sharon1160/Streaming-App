2704a08e3d914ae318c58a71a84592881b121f1b
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-19 12:58:24 -0500

    feat: remove token from url

--- app/src/main/java/com/example/streamingapp/data/network/SoundApiClient.kt
+++ app/src/main/java/com/example/streamingapp/data/network/SoundApiClient.kt
@@ -9,26 +9,23 @@ import retrofit2.http.Query
 
 interface SoundApiClient {
 
-    //@GET("search/text/?query={query}&token=$TOKEN&page=$PAGE&page_size=$PAGE_SIZE&format=json")
     @GET("search/text/?")
     suspend fun getAllSounds(
         @Query("query") query: String,
-        @Query("token") token: String = TOKEN,
         @Query("page") page: String = PAGE,
         @Query("page_size") pageSize: String = PAGE_SIZE,
-        @Query("format") format: String = "json"
+        @Query("format") format: String = FORMAT
     ): Response<SoundResponse>
 
     @GET("sounds/{id}/?")
     suspend fun getSound(
         @Path("id") id: String,
-        @Query("token") token: String = TOKEN,
-        @Query("format") format: String = "json"
+        @Query("format") format: String = FORMAT
     ): Response<DetailedSound>
 
     companion object {
-        const val TOKEN = "Frviaa2Vj7h8B2e13d4gZ5bOPr6jTljzJL34dbml"
         const val PAGE = "1"
         const val PAGE_SIZE = "2"
+        const val FORMAT = "json"
     }
 }
