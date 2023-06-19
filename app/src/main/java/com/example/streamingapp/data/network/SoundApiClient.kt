ab055ffbada0de1e730ce794a0826447ce3812f6
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-19 11:53:00 -0500

    feat: add queries and remove url

--- app/src/main/java/com/example/streamingapp/data/network/SoundApiClient.kt
+++ app/src/main/java/com/example/streamingapp/data/network/SoundApiClient.kt
@@ -1,20 +1,34 @@
 package com.example.streamingapp.data.network
 
 import com.example.streamingapp.data.model.DetailedSound
-import com.example.streamingapp.data.model.Sound
+import com.example.streamingapp.data.model.SoundResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SoundApiClient {
 
-    @GET(value = "search/text/?query={query}&token=$TOKEN")
-    suspend fun getAllSounds(@Query("query") query: String): Response<List<Sound>>
+    //@GET("search/text/?query={query}&token=$TOKEN&page=$PAGE&page_size=$PAGE_SIZE&format=json")
+    @GET("search/text/?")
+    suspend fun getAllSounds(
+        @Query("query") query: String,
+        @Query("token") token: String = TOKEN,
+        @Query("page") page: String = PAGE,
+        @Query("page_size") pageSize: String = PAGE_SIZE,
+        @Query("format") format: String = "json"
+    ): Response<SoundResponse>
 
-    @GET(value = "sounds/{id}/?token=$TOKEN")
-    suspend fun getSound(@Query("id") id: String): Response<DetailedSound>
+    @GET("sounds/{id}/?")
+    suspend fun getSound(
+        @Path("id") id: String,
+        @Query("token") token: String = TOKEN,
+        @Query("format") format: String = "json"
+    ): Response<DetailedSound>
 
     companion object {
         const val TOKEN = "Frviaa2Vj7h8B2e13d4gZ5bOPr6jTljzJL34dbml"
+        const val PAGE = "1"
+        const val PAGE_SIZE = "2"
     }
 }
