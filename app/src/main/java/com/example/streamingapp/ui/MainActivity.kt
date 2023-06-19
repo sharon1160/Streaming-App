27fcd7d60364ff749b11f7bd2d89499fbe1e3dd9
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-19 11:52:22 -0500

    feat: add android entry point annotation

--- app/src/main/java/com/example/streamingapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/streamingapp/ui/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.streamingapp.ui.navigation.graphs.RootNavigationGraph
 import com.example.streamingapp.ui.theme.StreamingAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
