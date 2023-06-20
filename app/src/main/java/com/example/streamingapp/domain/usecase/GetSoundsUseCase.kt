677e61591910217052041604155d6e81986892f8
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-19 22:07:15 -0500

    feat: add page size

--- app/src/main/java/com/example/streamingapp/domain/usecase/GetSoundsUseCase.kt
+++ app/src/main/java/com/example/streamingapp/domain/usecase/GetSoundsUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.streamingapp.domain.usecase
 
-import com.example.streamingapp.data.SoundRepository
+import com.example.streamingapp.data.repository.SoundRepository
 import com.example.streamingapp.data.model.Sound
 import javax.inject.Inject
 
@@ -8,5 +8,6 @@ class GetSoundsUseCase @Inject constructor(
     private val repository: SoundRepository
 ) {
 
-    suspend operator fun invoke(query: String): List<Sound>? = repository.getAllSounds(query)
+    suspend operator fun invoke(query: String, page: String, pageSize: String): List<Sound>? =
+        repository.getAllSounds(query, page, pageSize)
 }
