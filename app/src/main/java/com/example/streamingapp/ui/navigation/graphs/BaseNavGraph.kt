f4e40f44bdcfdab7afbd0cfecc420c5308a31c90
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-21 14:07:14 -0500

    feat: add music id

--- app/src/main/java/com/example/streamingapp/ui/navigation/graphs/BaseNavGraph.kt
+++ app/src/main/java/com/example/streamingapp/ui/navigation/graphs/BaseNavGraph.kt
@@ -2,8 +2,10 @@ package com.example.streamingapp.ui.navigation.graphs
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.streamingapp.ui.home.HomeScreen
 import com.example.streamingapp.ui.navigation.BottomBarItem
 import com.example.streamingapp.ui.player.PlayerScreen
@@ -18,8 +20,14 @@ fun HomeNavGraph(navController: NavHostController) {
         composable(route = BottomBarItem.Home.route) {
             HomeScreen(navController = navController)
         }
-        composable(route = BottomBarItem.Player.route) {
-            PlayerScreen()
+        composable(
+            route = "${BottomBarItem.Player.route}/{id}",
+            arguments = listOf(
+                navArgument("id") { type = NavType.IntType }
+            )
+        ) { backStackEntry ->
+            val id: Int? = backStackEntry.arguments?.getInt("id")
+            PlayerScreen(navController = navController, id = id)
         }
     }
 }
