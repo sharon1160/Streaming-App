eb80dce3efcb6ffec4011a392ec9842e17794cb3
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-22 11:26:11 -0500

    feat: add intent of media service

--- app/src/main/java/com/example/streamingapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/streamingapp/ui/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.example.streamingapp.ui
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.example.streamingapp.service.MediaService
 import com.example.streamingapp.ui.navigation.graphs.RootNavigationGraph
 import com.example.streamingapp.ui.theme.StreamingAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val intent = Intent(this, MediaService::class.java)
+        startForegroundService(intent)
         setContent {
             StreamingAppTheme {
                 Surface(
