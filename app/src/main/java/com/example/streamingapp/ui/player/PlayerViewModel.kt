ef89f640fe9a4f86aa437613798a2a747469691a
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-21 20:57:24 -0500

    feat: add select sound

--- app/src/main/java/com/example/streamingapp/ui/player/PlayerViewModel.kt
+++ app/src/main/java/com/example/streamingapp/ui/player/PlayerViewModel.kt
@@ -1,27 +1,75 @@
 package com.example.streamingapp.ui.player
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.Log
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
 import com.example.streamingapp.domain.usecase.GetSoundUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@UnstableApi
 @HiltViewModel
 class PlayerViewModel @Inject constructor(
-    private val getSoundUseCase: GetSoundUseCase
+    private val getSoundUseCase: GetSoundUseCase,
+    private val player: ExoPlayer
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(PlayerUiState())
     val uiState = _uiState.asStateFlow()
 
+    var updateSeekBarJob: Job? = null
+
+    private fun updateSlider() = viewModelScope.launch {
+        while (true) {
+            _uiState.update {
+                it.copy(currentPosition = player.currentPosition)
+            }
+            Log.e("aaa","${player.currentPosition}")
+            delay(1000)
+        }
+    }
+
+    init {
+        Log.e("depuracion","${player.playbackState}")
+    }
+
+    @UnstableApi
+    fun loadSound(url: String) {
+        val mediaItem = MediaItem.fromUri(Uri.parse(url))
+        player.setMediaItem(mediaItem)
+        player.prepare()
+    }
+
+    fun playPauseSound() {
+        if (player.isPlaying) {
+            player.pause()
+            updateSeekBarJob?.cancel()
+            updateSeekBarJob = null
+        } else {
+            player.play()
+            updateSeekBarJob?.cancel()
+            updateSeekBarJob = updateSlider()
+        }
+    }
+
     fun searchSoundById(id: Int) {
         viewModelScope.launch {
+            val selectedSound = getSoundUseCase.invoke(id = id.toString())
             _uiState.update {
                 it.copy(currentSound = getSoundUseCase.invoke(id = id.toString()))
+            }
+            selectedSound?.previews?.get("preview-hq-mp3")?.let {
+                loadSound(it)
             }
         }
     }
