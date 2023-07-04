555671f36ed3197390553c0d8b4360672b9143d6
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-07-04 15:30:43 -0500

    feat: add constants file

---
+++ app/src/main/java/com/example/streamingapp/service/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.streamingapp.service
+
+object Constants {
+    const val INITIAL_POSITION = 0L
+    const val REQUEST_CODE = 0
+    const val ID = 1
+    const val CHANNEL_NAME = "my_channel"
+    const val CHANNEL_ID = "my_channel_01"
+}
