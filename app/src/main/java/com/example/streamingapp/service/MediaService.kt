199b01c48220e91ead42f7d1b3af513ad7774bd8
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-07-04 15:31:01 -0500

    feat: add constants file

--- app/src/main/java/com/example/streamingapp/service/MediaService.kt
+++ app/src/main/java/com/example/streamingapp/service/MediaService.kt
@@ -21,44 +21,49 @@ class MediaService : MediaSessionService() {
     lateinit var context: Context
 
     @Inject
-    lateinit var notificationManager: NotificationManager
-
-    @Inject
     lateinit var mediaSession: MediaSession
 
+    @Inject
+    lateinit var notificationManager: NotificationManager
+
     @RequiresApi(Build.VERSION_CODES.O)
-    @UnstableApi
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val activityIntent = Intent(context, MainActivity::class.java).apply {
-            this.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-
-        val flagInmutable = PendingIntent.FLAG_IMMUTABLE
-        val pendingIntent = PendingIntent.getActivity(context, REQUEST_CODE, activityIntent, flagInmutable)
-
-        notificationManager.startNotificationService(this, pendingIntent)
-
-        return super.onStartCommand(activityIntent, flags, startId)
+    @androidx.annotation.OptIn(UnstableApi::class)
+    private fun startPlayerNotificationService(mainActivityIntent: Intent) {
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            context,
+            Constants.REQUEST_CODE,
+            mainActivityIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        notificationManager.startPlayerNotificationService(this, pendingIntent)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        mediaSession.run {
-            release()
-            if (player.playbackState != Player.STATE_IDLE) {
-                player.seekTo(INITIAL_POSITION)
-                player.playWhenReady = false
-                player.stop()
-            }
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val mainActivityIntent: Intent = Intent(context, MainActivity::class.java).apply {
+            this.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
+        startPlayerNotificationService(mainActivityIntent)
+        return super.onStartCommand(mainActivityIntent, flags, startId)
     }
 
     override fun onGetSession(
         controllerInfo: MediaSession.ControllerInfo
     ): MediaSession = mediaSession
 
-    companion object {
-        private const val INITIAL_POSITION = 0L
-        private const val REQUEST_CODE = 0
+    private fun destroyPlayer(player: Player) {
+        player.seekTo(Constants.INITIAL_POSITION)
+        player.playWhenReady = false
+        player.stop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        with(mediaSession) {
+            release()
+            if (player.playbackState != Player.STATE_IDLE) {
+                destroyPlayer(player)
+            }
+        }
     }
 }
